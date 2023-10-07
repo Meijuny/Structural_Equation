c54b439c4c34340a3ae85802c8cdc3d475796e5e
Meijun <meijuny21@gmail.com>
2023-10-07 08:56:00 +0200

    Function for defining missing value faster

---
+++ missing value function.R
@@ -0,0 +1,20 @@
+##Function to define 88 as NA
+defineNA_88<-function(data){
+        output<-data
+        for (i in seq_along(names(data))) {
+                output[,i]<-as.numeric(sub(88,NA,data[,i]))    
+        }
+        return(output)
+}
+
+ESS4_test[,2:4]<-defineNA_88(ESS4_test[,2:4])
+
+##function to define 77 as NA
+defineNA_77<-function(data){
+        output<-data
+        for (i in seq_along(names(data))) {
+                output[,i]<-as.numeric(sub(77,NA,data[,i]))    
+        }
+        return(output)
+}
+
